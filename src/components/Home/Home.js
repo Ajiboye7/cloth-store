90a12658c4aac073ca608f24cf8d36971a5cb3d5
Ajiboye <ajiboyemuyideen7@gmail.com>
2024-06-14 15:37:23 +0100

    update

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -2,12 +2,9 @@ import React from "react";
 import Category from "./Category";
 import "../styles/Home.css";
 import Hero from "./Hero";
-import Sales from "./Sales";
 import NewArrivals from "./NewArrival";
-import BestSellers from "./BestSellers";
 import SpecialOffers from './SpecialOffers';
 import ProductOfTheYear from "./ProductOfTheYear";
-import Shop from "../Pages/Shop";
 
 function Home() {
   return (
